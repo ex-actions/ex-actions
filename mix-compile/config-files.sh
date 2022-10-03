2a0fe46d74a30268f678c66a4bc94634bc5f2f06
Benjamin Stepp <me@benstepp.com>
2022-10-02 20:10:50 -0500

    just hash in elixir
    hashFiles doesn't support an array what a pile of garbage

--- mix-compile/config-files.sh
+++ mix-compile/config-files.sh
@@ -5,11 +5,15 @@ CONFIG_FILES=$(
     Logger.remove_backend(:console)
 
     Mix.Project.config_files()
-    |> Enum.map(&Kernel.inspect/1)
-    |> Enum.join(",")
-    |> then(&"[#{&1}]")
+    |> Enum.sort()
+    |> Enum.reduce(:crypto.hash_init(:sha256), fn file, hash ->
+      hash
+      |> :crypto.hash_update(file)
+      |> :crypto.hash_update(File.read!(file))
+    end)
+    |> :crypto.hash_final()
     |> IO.inspect()
   ' --no-compile --no-deps-check --no-archives-check --no-start
 )
 
-echo "::set-output name=config-files::$(echo $CONFIG_FILES | sed -n 's/^\"//pg' | sed -n 's/\"$//pg' | sed -n 's/\\//pg')"
+echo "::set-output name=config-files::$CONFIG_FILES"
