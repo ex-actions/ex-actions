d47f611672df84a90858c3e20d88b849bef750a7
Benjamin Stepp <me@benstepp.com>
2022-10-11 17:08:18 -0500

    Switch to esbuild
    holy it's way faster
    
    (also to support .exs as raw text files)

---
+++ esbuild.js
@@ -0,0 +1,17 @@
+const actions = ['mix-compile', 'mix-deps-compile', 'mix-deps-get', 'setup-mix']
+
+const config = {
+  bundle: true,
+  entryNames: '[dir]/dist/index',
+  entryPoints: actions.map((a) => `src/${a}/entry.ts`),
+  minify: true,
+  outbase: 'src',
+  outdir: 'actions',
+  platform: 'node',
+  target: 'node16',
+  watch: process.argv.indexOf('--watch') > -1,
+}
+
+require('esbuild')
+  .build(config)
+  .catch(() => process.exit(1))
