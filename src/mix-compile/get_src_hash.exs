10fb457240eba702b88d8f01d5b09a24bf501661
Benjamin Stepp <me@benstepp.com>
2023-07-03 10:51:35 -0500

    ensure :crypto is started
    also adds some additional debug information when in debug mode

--- src/mix-compile/get_src_hash.exs
+++ src/mix-compile/get_src_hash.exs
@@ -1,3 +1,12 @@
+:ok = Application.ensure_loaded(:crypto)
+{:ok, _} = Application.ensure_all_started(:crypto)
+
+if System.get_env("RUNNER_DEBUG", "0") == "1" do
+  IO.inspect(Application.loaded_applications(), limit: :infinity)
+  IO.inspect(Application.started_applications(), limit: :infinity)
+  IO.inspect(Mix.Utils.extract_files([Mix.Project.build_path()], "*"))
+end
+
 elixir_3p = [
   {:phoenix, [:heex]},
 ]
